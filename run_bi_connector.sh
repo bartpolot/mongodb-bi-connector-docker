8acf3ba0d1cda804e3aa8d49adb6f720ae5f119c
Bart Polot <bart@polot.org>
2020-10-15 11:55:41 +0200

    workaround bad bariable name

--- run_bi_connector.sh
+++ run_bi_connector.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 service rsyslog start
-/mongosqld/bin/mongodrdl  --uri "mongodb://$MONGODB_HOST:$MONGODB_PORT/$DATABASE?connect=direct" -c "$COLLECTION" > /tmp/src.drdl
+/mongosqld/bin/mongodrdl  --uri "mongodb://$MONGODB_HOST:$MONGODB__PORT/$DATABASE?connect=direct" -c "$COLLECTION" > /tmp/src.drdl
 /tmp/aggregate.py
-/mongosqld/bin/mongosqld --logPath /var/log/mongosqld_aggregated.log --mongo-uri mongodb://$MONGODB_HOST:$MONGODB_PORT/?connect=direct --addr 0.0.0.0:$LISTEN_PORT --schema /tmp/dst.drdl
+/mongosqld/bin/mongosqld --logPath /var/log/mongosqld_aggregated.log --mongo-uri mongodb://$MONGODB_HOST:$MONGODB__PORT/?connect=direct --addr 0.0.0.0:$LISTEN_PORT --schema /tmp/dst.drdl
 sleep 3600
